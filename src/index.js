6e9c1ca3b1c9263834200b39d7bed970af8aec3d
BZAgustin <abustoszanelli@gmail.com>
2023-11-23 11:24:57 -0300

    Add 25ms delay to setProperty

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ const heroSquares = document.querySelectorAll('.hero-square');
 const skillsSection = document.querySelectorAll('.card');
 
 const handleOnMouseMove = e => {
+  let timeoutId;
+  
   const { currentTarget: target } = e;
 
   const rect = target.getBoundingClientRect(),
                x = e.clientX - rect.left,
                y = e.clientY - rect.top;
 
-  target.style.setProperty('--mouse-x', `${x}px`);
-  target.style.setProperty('--mouse-y', `${y}px`);
+  clearTimeout(timeoutId);
+
+  timeoutId = setTimeout(() => {
+    target.style.setProperty('--mouse-x', `${x}px`);
+    target.style.setProperty('--mouse-y', `${y}px`);
+  }, 25);
 }
 
 for(const card of skillsSection) {
